344626990a11b94ca831eda655da6d095e15e498
Bernardo de Salles Cruz <168240480+devSalles@users.noreply.github.com>
2026-04-03 12:30:27 -0300

    Upload arquivo do projeto

---
+++ divEmpresas/src/main/java/divEmpresas/DivEmpresasApplication.java
@@ -0,0 +1,13 @@
+package divEmpresas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DivEmpresasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DivEmpresasApplication.class, args);
+	}
+
+}
---
+++ divEmpresas/src/test/java/divEmpresas/DivEmpresasApplicationTests.java
@@ -0,0 +1,13 @@
+package divEmpresas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DivEmpresasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
