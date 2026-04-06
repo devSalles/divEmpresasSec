89c9db56803fe3f4200f8473bd5294d25dfa9187
Bernardo Salles <bernardo.salles@GENOMA.COM>
2026-04-06 15:45:48 -0300

    Realizado a criação do metodo para salvar usuário + desenvolvimento da camada de criação e validação de token

--- divEmpresas/src/main/java/divEmpresas/core/security/TokenService.java
+++ divEmpresas/src/main/java/divEmpresas/core/security/TokenService.java
@@ -1,7 +1,64 @@
 package divEmpresas.core.security;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import divEmpresas.core.exceptions.CriacaoTokenException;
+import divEmpresas.core.exceptions.ValidacaoTokenException;
+import divEmpresas.entity.Usuario;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 
 @Service
 public class TokenService {
+
+    @Value("${api.key.value}")
+    private String secret;
+
+
+    public String createToken(Usuario usuario)
+    {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+
+            String token = JWT.create()
+                    .withIssuer("divEmpresasSec")
+                    .withSubject(usuario.getEmail())
+                    .withExpiresAt(timeExpiresToken())
+                    .sign(algorithm);
+            return token;
+
+        }
+        catch (JWTCreationException ex)
+        {
+            throw new CriacaoTokenException();
+        }
+    }
+
+    public String validateToken(String token)
+    {
+        try
+        {
+            Algorithm algorithm =Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                    .build()
+                    .verify(token)
+                    .getSubject();
+
+        }
+        catch (JWTVerificationException ex)
+        {
+            throw new ValidacaoTokenException();
+        }
+    }
+
+    public Instant timeExpiresToken()
+    {
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    }
 }
--- divEmpresas/src/main/java/divEmpresas/service/UserService.java
+++ divEmpresas/src/main/java/divEmpresas/service/UserService.java
@@ -1,21 +1,53 @@
 package divEmpresas.service;
 
 import divEmpresas.core.exceptions.EmailNaoEncontradoException;
+import divEmpresas.core.exceptions.GerenteNaoEncontradoException;
+import divEmpresas.core.exceptions.NaoPertenceAOrganizacaoException;
+import divEmpresas.core.exceptions.OrganizacaoNaoExistenteException;
+import divEmpresas.dto.user.UsuarioRequestDTO;
+import divEmpresas.dto.user.UsuarioResponseDTO;
+import divEmpresas.entity.Organizacao;
+import divEmpresas.entity.Usuario;
+import divEmpresas.repository.OrganizacaoRepository;
 import divEmpresas.repository.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class UserService implements UserDetailsService {
 
+    private final OrganizacaoRepository organizacaoRepository;
     private final UsuarioRepository usuarioRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return this.usuarioRepository.findByEmail(username).orElseThrow(EmailNaoEncontradoException::new);
+    }
+
+    public UsuarioResponseDTO salvarUsuario(UsuarioRequestDTO userDTO)
+    {
+        String senhaCriptografada = passwordEncoder.encode(userDTO.senha());
+        Organizacao organizacao = this.organizacaoRepository.findById(userDTO.idOrganizacao()).orElseThrow(OrganizacaoNaoExistenteException::new);
+
+        Usuario usuario = userDTO.toUsuario(organizacao);
+        usuario.setSenha(senhaCriptografada);
+
+        if(userDTO.idManager()!=null)
+        {
+            Usuario gerente = this.usuarioRepository.findById(userDTO.idManager()).orElseThrow(GerenteNaoEncontradoException::new);
+
+            if(!gerente.getOrganizacao().getId().equals(organizacao.getId()))
+            {
+                throw new NaoPertenceAOrganizacaoException();
+            }
+        }
+
+        return UsuarioResponseDTO.fromUsuario(usuario);
     }
 }
